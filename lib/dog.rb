041eec4c84a0953d21d8438f9329633d548335a7
ryan m <contact@ryanmeek.com>
2020-01-28 21:45:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,7 +30,7 @@ class Dog
   def self.create(name:, breed:) self.new(name: name, breed: breed).save; self end
 
   def update
-    DB[:conn].execute('UPDATE dogs SET name = ?, breed = ?, WHERE id = ?', self.name, self.breed, self.id)
+    DB[:conn].execute('UPDATE dogs SET name = ?, breed = ? WHERE id = ?', self.name, self.breed, self.id)
   end
 
 end
