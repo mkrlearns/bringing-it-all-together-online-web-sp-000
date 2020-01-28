fa76eb45f3ea3343f2d5ab2e409d632c58ee3c36
ryan m <contact@ryanmeek.com>
2020-01-28 21:37:22 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,32 @@
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    DB[:conn].execute(<<~SQL
+      CREATE TABLE IF NOT EXISTS dogs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+    )
+  end
+
+  def self.drop_table() DB[:conn].execute('DROP TABLE IF EXISTS dogs') end
+
+  def save
+    self.update; return if self.id
+    DB[:conn].execute('INSERT INTO dogs (name, breed) VALUES (?, ?)', self.name, self.breed)
+    @id = DB[:conn].execute('SELECT last_insert_rowid() FROM dogs')[0][0]
+    self
+  end
+
+
+
+end
