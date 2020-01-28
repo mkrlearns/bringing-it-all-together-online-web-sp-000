21a4f87a0e10b7150b70e2b2a5789532ec9f95d5
ryan m <contact@ryanmeek.com>
2020-01-28 22:05:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,11 +36,11 @@ class Dog
   def self.new_from_db(row) self.new(id: row[0], name: row[1], breed: row[2]) end
 
   def self.find_by_name(name)
-    self.db_query('SELECT * FROM dogs WHERE name = ? LIMIT 1', name).first
+    self.db_query('SELECT * FROM dogs WHERE name = ? LIMIT 1', name)
   end
 
   def self.find_by_id(id)
-    self.db_query('SELECT * FROM dogs WHERE id = ? LIMIT 1', id).first
+    self.db_query('SELECT * FROM dogs WHERE id = ? LIMIT 1', id)
   end
 
   def self.find_or_create_by(name:, breed:)
